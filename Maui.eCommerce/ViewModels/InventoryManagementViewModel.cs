cf2fac2429a1e1972218926234686cb39b3a803b
Chris Mills <crmills@fsu.edu>
2025-02-10 14:00:18 -0500

    I fixed the delete UI issue

--- Maui.eCommerce/ViewModels/InventoryManagementViewModel.cs
+++ Maui.eCommerce/ViewModels/InventoryManagementViewModel.cs
@@ -2,27 +2,45 @@
 using Spring2025_Samples.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Maui.eCommerce.ViewModels
 {
-    public class InventoryManagementViewModel
+    public class InventoryManagementViewModel : INotifyPropertyChanged
     {
         public Product? SelectedProduct { get; set; }
         private ProductServiceProxy _svc = ProductServiceProxy.Current;
-        public List<Product?> Products
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (propertyName is null)
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<Product?> Products
         {
             get
             {
-                return _svc.Products;
+                return new ObservableCollection<Product?>(_svc.Products);
             }
         }
 
         public Product? Delete()
         {
-            return _svc.Delete(SelectedProduct?.Id ?? 0);
+            var item = _svc.Delete(SelectedProduct?.Id ?? 0);
+            NotifyPropertyChanged("Products");
+            return item;
         }
     }
 }
